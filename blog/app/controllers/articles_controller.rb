d5c0449f5242470f13b0cf0dad22184852d9ab70
myHelloWord <zhangqiuyuan@gmail.com>
2014-05-02 21:00:27 +1200

    create article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,14 @@ class ArticlesController < ApplicationController
   def new
   end
   def create
-    render plain: params[:article].inspect
+   # render plain: params[:article].inspect
+    @article = Article.new()
+    @article.save
+    redirect_to @article  
   end
+  private
+    def article_parms
+      params.require(:article).permit(:title, :text)
+    end
+
 end
